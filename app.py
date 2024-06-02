9d100dca42c27b0a8fa8deff2d147b6e83e6cb24
fadhluuu <farisfadhlur03@gmail.com>
2024-06-03 02:01:12 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return 'Hello, Batikpedia!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
