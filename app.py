8fe7672cdc117d95c8d0e6134efc634fcfc3f76d
farisfadhlur03@gmail.com <farisfadhlur03@gmail.com>
2024-06-04 15:53:12 +0700

    testing

--- app.py
+++ app.py
@@ -1,85 +1,116 @@
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import glob
-import re
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
+from tensorflow.python.keras.models import Sequential, load_model
+from tensorflow.python.keras.layers import Dense, LeakyReLU, Dropout, Activation
+from tensorflow.python.keras.applications import Xception
+from PIL import Image
+from io import BytesIO
+from IPython.display import display, clear_output
 import numpy as np
+import os
 
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-
-# Define a flask app
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
-MODEL_PATH = 'models/model_resnet.h5'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Load your trained model
-model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
+db = SQLAlchemy(app)
 
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-print('Model loaded. Check http://127.0.0.1:5000/')
+def create_pre_trained_model():
+    pre_trained_model = Xception(
+        input_shape=(224, 224, 3),
+        include_top=False,
+        weights='imagenet',
+        pooling="max"
+    )
+
+    pre_trained_model.trainable = False
+
+    return pre_trained_model
+
+pre_trained_model = create_pre_trained_model()
+
+def create_final_model(pre_trained_model):
+    inputs = Input(shape=(224, 224, 3))
+    x = pre_trained_model(inputs)
+    x = Dropout(0.3)(x)
+    outputs = Dense(units=7, activation='softmax')(x)
+    model = model(inputs=inputs, outputs=outputs)
+
+    model.compile(optimizer="adam",
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"])
+
+    return model
+
+model = create_final_model(pre_trained_model)
+
+checkpoint_filepath = 'models/checkpoint.weights.h5'
+model.load_weights(checkpoint_filepath)
 
 
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
+# Get class labels
+TRAIN_DIR = 'TRAIN/'
+class_labels = sorted(os.listdir(TRAIN_DIR))
 
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
-
-    preds = model.predict(x)
-    return preds
-
+# Define average thresholds for each class
+average_thresholds = {
+    "Batik Insang": 0.84,
+    "Batik Cendrawasih": 0.78,
+    "Batik Kawung": 0.86,
+    "Batik Megamendung": 0.91,
+    "Batik Parang": 0.83,
+    "Batik Poleng": 0.84,
+    "Batik Tambal": 0.89
+}
 
 @app.route('/', methods=['GET'])
 def index():
-    # Main page
     return render_template('index.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
+def predict_image(image):
+    img = Image.open(BytesIO(image)).resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
+    predictions = model.predict(img_array)
+    predicted_probabilities = predictions[0]
 
-        # Make prediction
-        preds = model_predict(file_path, model)
+    return predicted_probabilities
 
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        return result
-    return None
+# Define the function to handle image upload and prediction
+def on_upload_change(change):
+    clear_output()
+    for name, file_info in change['new'].items():
+        img = file_info['content']
+        display(Image.open(BytesIO(img)).resize((224, 224)))
 
+        predicted_probabilities = predict_image(img)
+
+        # Sort the probabilities in descending order
+        sorted_indices = np.argsort(predicted_probabilities)[::-1]
+        sorted_probabilities = predicted_probabilities[sorted_indices]
+        sorted_labels = [class_labels[i] for i in sorted_indices]
+
+        # Determine if the prediction is recognized or unrecognized
+        is_recognized = False
+        for label, prob in zip(sorted_labels, sorted_probabilities):
+            if prob >= average_thresholds[label]:
+                is_recognized = True
+                break
+
+        if is_recognized:
+            print(f"Predicted Label: {sorted_labels[0]}")
+            print("Predicted Probabilities:")
+            for label, prob in zip(sorted_labels, sorted_probabilities):
+                print(f"{label}: {prob:.2f}")
+        else:
+            print("Unrecognized")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
