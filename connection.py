96638fbd9e43533a63bbb2f9e5c2570934bed534
Faizal_Husain_Adiasha <adihusain0@gmail.com>
2024-06-06 07:55:41 +0700

    remove postgresql to env

--- connection.py
+++ connection.py
@@ -1,7 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+import os
+from dotenv import load_dotenv
+# read from db from environment variable
 
+load_dotenv()
 
-engine = create_engine("postgresql://team_pokin:pokin5432!@34.128.115.20/batikpedia")
+database_url = os.getenv('DATABASE_URL')
+
+print(database_url)
+engine = create_engine(database_url)
+
+# engine = create_engine("postgresql://team_pokin:pokin5432!@34.128.115.20/batikpedia")
 
 session = Session(engine)
